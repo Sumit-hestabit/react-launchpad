3343fdc110976f30a35f95f303e82a163ce2fb1b
sumit-hestabit <sumit.choudhary@hestabit.in>
2021-10-18 23:20:35 +0530

    parents-to-child

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react'
-// import './App.css'
-//import '../Todo.scss';
-import NewTaskForm from './component/NewTaskForm';
-import TaskList from './component/TaskList';
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  // 2
-  return (
-    <div className="container">
-      <h1 className="title">Todo List</h1>
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  )
+import GameMgr from '../src/component/GameMgr';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GameMgr />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
